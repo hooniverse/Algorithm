73d7f84847ede61391d4ba7df9f7f7411fb08b09
이성훈 <121534585+hooniverse@users.noreply.github.com>
2024-12-03 21:59:03 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 20288 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] result = new boolean[n + 1];
+
+
+        result[0] = true;
+        result[1] = true;
+
+        for(int i = 2; i * i <= n; i++) {
+
+            if(!result[i]) {
+                for(int j = i * i; j <= n; j += i) {
+                    result[j] = true;
+                }
+            }
+        }
+
+        for(int i = m; i <= n; i++) {
+            if(!result[i]) sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
