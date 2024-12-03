eba0d260258fe19946f7018a75982a7a23fdf612
이성훈 <121534585+hooniverse@users.noreply.github.com>
2024-12-04 00:07:22 +0900

    [Silver III] Title: 모든 순열, Time: 1640 ms, Memory: 55732 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int n;
+    static int[] result;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        result = new int[n];
+        visited = new boolean[n];
+
+        back(0);
+    }
+
+    public static void back(int depth){
+        if(depth == n){
+            for(int i:result){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                result[depth] = i+1;
+                back(depth+1);
+                visited[i] = false;
+            }
+        }
+
+    }
+}
